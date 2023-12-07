b854e3df543c2239f23b2c1eb77082b658444d0d
Siddhartha Mishra <mailbox.siddharthamishra@gmail.com>
2023-12-07 14:03:22 +0530

    feat: add authentication modal using supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={font.className}>
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -13,6 +14,7 @@ interface HeaderProps {
 }
 
 const Header = ({ children, className }: HeaderProps) => {
+  const authModal = useAuthModal();
   const router = useRouter();
   return (
     <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`)}>
@@ -22,32 +24,52 @@ const Header = ({ children, className }: HeaderProps) => {
             onClick={() => router.back()}
             className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
-            <RxCaretLeft className="text-white" size={35} />
+            <RxCaretLeft
+              className="text-white"
+              size={35}
+            />
           </button>
           <button
             onClick={() => router.forward()}
             className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
-            <RxCaretRight className="text-white" size={35} />
+            <RxCaretRight
+              className="text-white"
+              size={35}
+            />
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
           <button className="p-2 bg-white rounded-full flex items-center justify-center hover:opacity-75 transition">
-            <HiHome size={20} className="text-black" />
+            <HiHome
+              size={20}
+              className="text-black"
+            />
           </button>
           <button className="p-2 bg-white rounded-full flex items-center justify-center hover:opacity-75 transition">
-            <BiSearch size={20} className="text-black" />
+            <BiSearch
+              size={20}
+              className="text-black"
+            />
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button className="bg-transparent font-medium text-neutral-300">
+              <Button
+                onClick={authModal.onOpen}
+                className="bg-transparent font-medium text-neutral-300"
+              >
                 Sign up
               </Button>
             </div>
             <div>
-              <Button className="bg-white px-6 py-2">Log in</Button>
+              <Button
+                onClick={authModal.onOpen}
+                className="bg-white px-6 py-2"
+              >
+                Log in
+              </Button>
             </div>
           </>
         </div>
