05bcd187fb0e311d84af5a795d5397c4f91e12e6
Siddhartha Mishra <mailbox.siddharthamishra@gmail.com>
2023-12-10 01:08:41 +0530

    feat: fetch and display songs

--- types.ts
+++ types.ts
@@ -1,5 +1,13 @@
-import { LargeNumberLike } from "crypto";
 import Stripe from "stripe";
+
+export interface Song {
+  id: string;
+  user_id: string;
+  title: string;
+  author: string;
+  song_path: string;
+  image_path: string;
+}
 
 export interface UserDetails {
   id: string;
