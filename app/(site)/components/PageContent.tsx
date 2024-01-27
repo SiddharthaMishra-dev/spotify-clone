fd96d392b634b27d4089b21c54255953390141a9
SiddharthaMishra-dev <mailbox.siddharthamishra@gmail.com>
2024-01-27 15:46:20 +0530

    ui: add upload song button for mobile devices

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { AiOutlinePlus } from "react-icons/ai";
+
 import SongItem from "@/components/SongItem";
+import useAuthModal from "@/hooks/useAuthModal";
 import useOnPlay from "@/hooks/useOnPlay";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+
 import { Song } from "@/types";
 
 interface PageContentProps {
@@ -10,13 +16,24 @@ interface PageContentProps {
 
 const PageContent = ({ songs }: PageContentProps) => {
   const onPlay = useOnPlay(songs);
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+
+  const { user } = useUser();
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+    return uploadModal.onOpen();
+  };
 
   if (songs.length === 0) {
     return <div className="mt-d text-neutral-400">No songs available</div>;
   }
   return (
-    <div
-      className="
+    <>
+      <div
+        className="
     grid
     grid-cols-2
     sm:grid-cols-3
@@ -27,15 +44,25 @@ const PageContent = ({ songs }: PageContentProps) => {
     gap-4
     mt-4
     "
-    >
-      {songs.map((item) => (
-        <SongItem
-          key={item.id}
-          onClick={(id: string) => onPlay(id)}
-          data={item}
+      >
+        {songs.map((item) => (
+          <SongItem
+            key={item.id}
+            onClick={(id: string) => onPlay(id)}
+            data={item}
+          />
+        ))}
+      </div>
+      <div
+        onClick={onClick}
+        className="md:hidden absolute bottom-20 left-10 bg-white rounded-full p-3 flex justify-center items-center hover:opacity-75 transition"
+      >
+        <AiOutlinePlus
+          className="text-black font-bold"
+          size={20}
         />
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
